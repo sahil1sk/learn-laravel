35157731e4543db408b8fdd70d57978ad571915e
sahil1sk <khannasahil303@gmail.com>
2021-04-23 18:58:26 -0700

    routing

--- app/Http/Controllers/Site.php
+++ app/Http/Controllers/Site.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class Site extends Controller
 {
-    public function first() {
-        // echo "<h1>Welcome to First Method</h1>";
+    public function first($id = 1) {
+        echo "<h1>Welcome to First Method $id</h1>";
         return view("first");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/first', [Site::class, "first"]);
+Route::get("services/{id}", function($id) {
+    echo "<h1>ID: $id </h1>";
+})->where("id", "[0-9]+"); // id will accept only integer values
+
+Route::get("services/{id}/{name}", function($id, $name) {
+    echo "<h1>ID: $id </h1>";
+    echo "<h1>Name: $name </h1>";
+})->where(["id" => "[0-9]+", "name" => "[a-zA-Z]+"]); // name contains only a to z characters not integers 
+
+
+Route::get("/list-view", function() {
+    echo "<h1>This is list view page</h1>";
+});
+
+Route::get("/add-form", function() {
+    return redirect("/list-view");
+});
+
+
+Route::get("/about-us", function() {
+    echo "<h1>This is simple message </h1>";
+});
+
+Route::redirect("/products", "/about-us");
+
+// ? helps to set it as optional parameter
+Route::get('/first/{id?}', [Site::class, "first"]); 
