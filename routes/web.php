bdae439ebde449f0ef8c1eef380b2f70d9bbdfb3
sahil1sk <khannasahil303@gmail.com>
2021-05-02 07:22:56 -0700

    url() route() action() methods

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,34 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 use App\Models\Student;
 
+Route::get("test", function() {
+    echo url("call/5") . "<br>";
+    echo url()->current() . "<br>";
+    echo url()->full() . "<br>";
+    
+    echo URL::full() . "<br>"; // Using url facades
+    
+    // In route we are able to pass only named routes
+    echo route("call", ["id" => 3]) . "<br>"; // In route function we are able to pass only named routes 
+    echo route("sample") . "<br>";
+
+    // -- In action we are able to pass only Controllers routes
+    echo action([StudentController::class, "addStudent"], ["id" => 2 ]);
+});
+
+Route::get("student/{id}", [StudentController::class, "addStudent"]);
+
+Route::get("call/{id}", function($id) {
+    echo "This is a call route $id";
+})->name("call");
+
+Route::get("sample", function() {
+    echo "This is a sample route";
+})->name("sample");
 
 // this will return the data according to student id
 Route::get('/student/{studentID}', function(Student $studentID) {
