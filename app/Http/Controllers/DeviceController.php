eacf6c4ef35f38a352a3b6a9c01b42bb510e089b
sahil1sk <khannasahil303@gmail.com>
2021-05-18 16:52:30 -0700

    global scope

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Device;
 use Illuminate\Http\Request;
+use App\Scopes\StatusScope;
 
 class DeviceController extends Controller
 {
 
     // -------- local scope
     public function getStudentsData() {
-        $devices = Device::whereStatus(0)->get();
+        // $devices = Device::all(); // so here automatically the codition of global scope will aplied
+        
+        // to get data without global scope
+        $devices = Device::withoutGlobalScope(StatusScope::class)->get(); 
+        
+
+        // $devices = Device::whereStatus(0)->get();
 
         echo"<pre>";
         print_r($devices);
--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\StatusScope;
 
 class Device extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new StatusScope);
+    }
 
     protected function scopeWhereStatus($query, $val) {
         return $query->where("status", $val);
